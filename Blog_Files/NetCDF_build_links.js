cc3a3f7d8cb06043e40b99a0d7f09dd59eabd6c8
PittmanRW@gmail.com <PittmanRW@gmail.com@62150d73-fb99-cb60-cb19-b2a44c365dcc>
2011-11-28 21:21:55 +0000

    Added link to Part 2.
    Former-commit-id: 290491a6acee7a1a0a08e6c30379d49329e59048

--- Blog_Files/NetCDF_build_links.js
+++ Blog_Files/NetCDF_build_links.js
@@ -1,6 +1,6 @@
 document.write("<p>Other posts in this series:" + 
 "    <ul>" + 
 "        <li><a href=\"http://radarwaveimaging.blogspot.com/2011/11/building-netcdf-software-part-1.html\">Part 1</a></li>" + 
-"        <li>Part 2</li>" + 
+"        <li><a href=\"http://radarwaveimaging.blogspot.com/2011/11/building-netcdf-software-part-2.html\">Part 2</a></li>" + 
 "    </ul>" + 
 "</p>")
