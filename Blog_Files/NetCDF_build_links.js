fe536555ba2eef4fa36356e165670d61eaf4cedd
PittmanRW@gmail.com <PittmanRW@gmail.com@62150d73-fb99-cb60-cb19-b2a44c365dcc>
2011-11-28 20:24:34 +0000

    Added line breaks back in. The problem was that there were non-escaped quotes in the hyperlink.
    Former-commit-id: e1f9754f803f89821bb34d245845dce3fdb48bff

--- Blog_Files/NetCDF_build_links.js
+++ Blog_Files/NetCDF_build_links.js
@@ -1,1 +1,6 @@
-document.write("<p>Other posts in this series:<ul><li><a href=\"http://radarwaveimaging.blogspot.com/2011/11/building-netcdf-software-part-1.html\">Part 1</a></li><li>Part 2</li></ul></p>")
+document.write("<p>Other posts in this series:" + 
+"    <ul>" + 
+"        <li><a href=\"http://radarwaveimaging.blogspot.com/2011/11/building-netcdf-software-part-1.html\">Part 1</a></li>" + 
+"        <li>Part 2</li>" + 
+"    </ul>" + 
+"</p>")
