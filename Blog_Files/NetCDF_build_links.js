57155b38bce7a41d56d00a990770aab6aee66ca0
PittmanRW@gmail.com <PittmanRW@gmail.com@62150d73-fb99-cb60-cb19-b2a44c365dcc>
2011-11-28 21:23:30 +0000

    Changed note
    Former-commit-id: a53e5b87bf4d116d0a693ced19cdee37187385ee

--- Blog_Files/NetCDF_build_links.js
+++ Blog_Files/NetCDF_build_links.js
@@ -1,4 +1,4 @@
-document.write("<p>Other posts in this series:" + 
+document.write("<p><span style="font-style:italic;">Links to all posts in this series:</span>" + 
 "    <ul>" + 
 "        <li><a href=\"http://radarwaveimaging.blogspot.com/2011/11/building-netcdf-software-part-1.html\">Part 1</a></li>" + 
 "        <li><a href=\"http://radarwaveimaging.blogspot.com/2011/11/building-netcdf-software-part-2.html\">Part 2</a></li>" + 
