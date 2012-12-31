a5a8d7c452bbaa17cbb7dc427b9bd228f70268a7
PittmanRW@gmail.com <PittmanRW@gmail.com@62150d73-fb99-cb60-cb19-b2a44c365dcc>
2012-12-31 21:40:08 +0000

    Tool to forcibly reboot - may work better than shutdown.exe.
    Former-commit-id: ac48c6949f81520360f6366e209e38a3c6afd19c

---
+++ WMI_Reboot/WMI_Reboot/Module1.vb
@@ -0,0 +1,31 @@
+﻿Option Explicit On
+Option Strict On
+Imports System.Management
+
+Module Module1
+
+    Sub Main()
+        Dim strComputer As String = "."
+        Dim options As New System.Management.ConnectionOptions
+        options.Impersonation = ImpersonationLevel.Impersonate
+        options.EnablePrivileges = True
+        Dim ms As New ManagementScope("\\" & strComputer & "\root\CIMV2", options)
+        Dim q As New SelectQuery("SELECT * FROM Win32_OperatingSystem")
+        Dim search As New ManagementObjectSearcher(ms, q)
+
+        Console.Write("System will forcibly reboot in ")
+        For sec As Integer = 10 To 1 Step -1
+            Console.Write(sec.ToString & ".. ")
+            Threading.Thread.Sleep(1000)
+        Next
+
+        ' enum each entry for Win32_OperatingSystem and call WMI reboot method
+        Dim args() As Object = {6, 0}
+        For Each info As ManagementObject In search.Get()
+            info.InvokeMethod("Win32Shutdown", args)
+            'Console.WriteLine(info.GetText(TextFormat.Mof))
+        Next
+
+    End Sub
+
+End Module
---
+++ WMI_Reboot/WMI_Reboot/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("WMI_Reboot")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Oregon State University")> 
+<Assembly: AssemblyProduct("WMI_Reboot")> 
+<Assembly: AssemblyCopyright("Copyright © Oregon State University 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ed422656-0dd6-4e91-97cc-d08f0e7e7903")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
