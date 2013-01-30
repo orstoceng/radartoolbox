2989eee640581433ea43923813c8fc44223875be
PittmanRW@gmail.com <PittmanRW@gmail.com@62150d73-fb99-cb60-cb19-b2a44c365dcc>
2013-01-30 23:45:06 +0000

    New tool to try out getting the output from a command window.
    Former-commit-id: 5446bd0877a59ebf2350defc96186ba3858b11d2

---
+++ CmdRedirect/Form1.vb
@@ -0,0 +1,123 @@
+Public Class Form1
+    Inherits System.Windows.Forms.Form
+
+#Region " Windows Form Designer generated code "
+
+    Public Sub New()
+        MyBase.New()
+
+        'This call is required by the Windows Form Designer.
+        InitializeComponent()
+
+        'Add any initialization after the InitializeComponent() call
+
+    End Sub
+
+    'Form overrides dispose to clean up the component list.
+    Protected Overloads Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing Then
+            If Not (components Is Nothing) Then
+                components.Dispose()
+            End If
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    Friend WithEvents btnSend As System.Windows.Forms.Button
+    Friend WithEvents txtCommand As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtResults As System.Windows.Forms.TextBox
+    <System.Diagnostics.DebuggerStepThrough()> Private Sub InitializeComponent()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.txtResults = New System.Windows.Forms.TextBox()
+        Me.txtCommand = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(8, 8)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(96, 23)
+        Me.btnSend.TabIndex = 0
+        Me.btnSend.Text = "Send Command"
+        '
+        'txtResults
+        '
+        Me.txtResults.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtResults.Location = New System.Drawing.Point(8, 84)
+        Me.txtResults.Multiline = True
+        Me.txtResults.Name = "txtResults"
+        Me.txtResults.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtResults.Size = New System.Drawing.Size(496, 299)
+        Me.txtResults.TabIndex = 1
+        Me.txtResults.WordWrap = False
+        '
+        'txtCommand
+        '
+        Me.txtCommand.Location = New System.Drawing.Point(240, 8)
+        Me.txtCommand.Multiline = True
+        Me.txtCommand.Name = "txtCommand"
+        Me.txtCommand.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.txtCommand.Size = New System.Drawing.Size(260, 70)
+        Me.txtCommand.TabIndex = 2
+        Me.txtCommand.Text = "dir"
+        Me.txtCommand.WordWrap = False
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(120, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(112, 23)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Commands To Send:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleBaseSize = New System.Drawing.Size(5, 13)
+        Me.ClientSize = New System.Drawing.Size(512, 388)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtCommand)
+        Me.Controls.Add(Me.txtResults)
+        Me.Controls.Add(Me.btnSend)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+#End Region
+
+    Private Sub btnSend_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSend.Click
+        'Dim CMDThread As New Threading.Thread(AddressOf CMDAutomate)
+        'CMDThread.Start()
+        CMDAutomate()
+    End Sub
+    Private Sub CMDAutomate()
+        Dim myprocess As New Process
+        Dim StartInfo As New System.Diagnostics.ProcessStartInfo
+        StartInfo.FileName = "cmd" 'starts cmd window
+        StartInfo.RedirectStandardInput = True
+        StartInfo.RedirectStandardOutput = True
+        StartInfo.UseShellExecute = False 'required to redirect
+        myprocess.StartInfo = StartInfo
+        myprocess.Start()
+        Dim SR As System.IO.StreamReader = myprocess.StandardOutput
+        Dim SW As System.IO.StreamWriter = myprocess.StandardInput
+        SW.WriteLine(txtCommand.Text) 'the command you wish to run.....
+        SW.WriteLine("exit") 'exits command prompt window
+        txtResults.Text = SR.ReadToEnd
+        SW.Close()
+        SR.Close()
+    End Sub
+End Class
